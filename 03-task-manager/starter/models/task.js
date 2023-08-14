4a6d2d8aabde163a72a6dd0aa6e6f0223a9c5dce
Rakshit rajput <Rakshitrajput212@gmail.com>
2023-08-14 15:28:17 +0530

    task manager completed

--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -7,7 +7,11 @@ const TaskSchema = new mongoose.Schema({
     trim:true,
     maxlength:[20,'name can not be more than 20 characters']
 },
-    completed:Boolean,
+    completed:{
+        type:Boolean,
+        default:false
+    }
+    
 })
 
 module.exports = mongoose.model('Task',TaskSchema)
