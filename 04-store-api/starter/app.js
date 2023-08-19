e8c4823eb0f052149cda0167b700d29e758e40bf
Rakshit rajput <Rakshitrajput212@gmail.com>
2023-08-19 23:38:51 +0530

    not much

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,43 @@
-console.log('04 Store API')
+require('dotenv').config();
+require('express-async-errors')
+//async errors
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect')
+
+const productsRouter = require('./routes/products')
+
+const notFoundMiddleware = require('./middleware/error-handler')
+const errorMiddleware = require('./middleware/error-handler')
+
+app.use(express.json())
+
+//routes
+
+app.get('/',(req,res)=>{
+    res.send('<h1>store API</h1><a href="/api/v1/products">Go to products</a>')
+})
+
+app.use('/api/v1/products',productsRouter)
+
+
+//products routes
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+
+const port = process.env.PORT || 3000
+
+const start = async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`Server is listening port ${port}...`))
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
